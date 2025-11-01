c99ccffaf386e6b484275288c078a8ddbf8733f8
wildmaster84 <46942741+wildmaster84@users.noreply.github.com>
2025-10-31 20:54:52 -0400

    moved face logic into dedi class

--- src/main/java/engine/rendering/Renderer.java
+++ src/main/java/engine/rendering/Renderer.java
@@ -2,6 +2,8 @@ package engine.rendering;
 
 import engine.world.World;
 import engine.world.Chunk;
+import engine.rendering.FaceRenderer.FaceDirection;
+import engine.rendering.FaceRenderer.FaceVertices;
 import engine.world.Block;
 import engine.world.BlockType;
 import org.lwjgl.opengl.*;
@@ -20,93 +22,6 @@ public class Renderer {
 
     private int vaoId;
     private int vboId;
-
-    public enum FaceVertices {
-        TOP(new float[]{0,1,0,0,0,1,1,0,1,0,1,1,1,1,1,0,1,0,0,0,1,1,1,1,1,0,1,1,0,1}),
-        BOTTOM(new float[]{0,0,0,0,0,1,0,0,1,0,1,0,1,1,1,0,0,0,0,0,1,0,1,1,1,0,0,1,0,1}),
-        LEFT(new float[]{0,0,0,0,0,0,0,1,1,0,0,1,1,1,1,0,0,0,0,0,0,1,1,1,1,0,1,0,0,1}),
-        RIGHT(new float[]{1,0,0,0,0,1,0,1,1,0,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,0,0,1}),
-        FRONT(new float[]{0,0,0,0,0,1,0,0,1,0,1,1,0,1,1,0,0,0,0,0,1,1,0,1,1,0,1,0,0,1}),
-        BACK(new float[]{0,0,1,0,0,1,0,1,1,0,1,1,1,1,1,0,0,1,0,0,1,1,1,1,1,0,1,1,0,1});
-    	
-    	private final float[] vertices;
-
-        FaceVertices(float[] vertices) {
-            this.vertices = vertices;
-        }
-
-        public static float[] get(int face) {
-        	switch(face) {
-        	case 0: {
-        		return TOP.vertices;
-        	}
-        	case 1: {
-        		return BOTTOM.vertices;
-        	}
-        	case 2: {
-        		return LEFT.vertices;
-        	}
-        	case 3: {
-        		return RIGHT.vertices;
-        	}
-        	case 4: {
-        		return FRONT.vertices;
-        	}
-        	case 5: {
-        		return BACK.vertices;
-        	}
-        	default: {
-        		return new float[]{};
-        	}
-        	}
-        }
-    };
-
-    public enum FaceDirection {
-        TOP(0, 1, 0),
-        BOTTOM(0, -1, 0),
-        LEFT(-1, 0, 0),
-        RIGHT(1, 0, 0),
-        FRONT(0, 0, -1),
-        BACK(0, 0, 1);
-
-        public final int dx, dy, dz;
-
-        FaceDirection(int dx, int dy, int dz) {
-            this.dx = dx;
-            this.dy = dy;
-            this.dz = dz;
-        }
-
-        public int[] toArray() {
-            return new int[] { dx, dy, dz };
-        }
-        public static int[] get(int face) {
-        	switch(face) {
-        	case 0: {
-        		return TOP.toArray();
-        	}
-        	case 1: {
-        		return BOTTOM.toArray();
-        	}
-        	case 2: {
-        		return LEFT.toArray();
-        	}
-        	case 3: {
-        		return RIGHT.toArray();
-        	}
-        	case 4: {
-        		return FRONT.toArray();
-        	}
-        	case 5: {
-        		return BACK.toArray();
-        	}
-        	default: {
-        		return new int[]{};
-        	}
-        	}
-        }
-    }
 
     public Renderer(World world, Camera camera) {
         this.world = world;
